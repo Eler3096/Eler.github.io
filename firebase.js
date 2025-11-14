c354dbe6433f7a7ce38fc6edd79eafb5474be95b
Eler <eler30.01@gmail.com>
2025-11-14 16:33:13 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,19 @@
-<!-- firebase.js -->
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-storage.js"></script>
+// Importar SDK v8 desde HTML, NO aquí.
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyApdnTXHE7O6AoU9ldEV4yzsCVBGdsWuRg",
-    authDomain: "web-appsmart-technology.firebaseapp.com",
-    projectId: "web-appsmart-technology",
-    storageBucket: "web-appsmart-technology.appspot.com",
-    messagingSenderId: "595885713972",
-    appId: "1:595885713972:web:9ab9dd4072db0101207861"
-  };
+// Configuración Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyApdnTXHE7O6AoU9ldEV4yzsCVBGdsWuRg",
+  authDomain: "web-appsmart-technology.firebaseapp.com",
+  projectId: "web-appsmart-technology",
+  storageBucket: "web-appsmart-technology.appspot.com",
+  messagingSenderId: "595885713972",
+  appId: "1:595885713972:web:9ab9dd4072db0101207861"
+};
 
-  firebase.initializeApp(firebaseConfig);
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-  const storage = firebase.storage();
-</script>
+// REFERENCIAS
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
