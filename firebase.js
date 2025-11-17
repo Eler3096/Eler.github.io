ba789a5f7729a76f349c19fb0fa8724251a47bd2
Eler <eler30.01@gmail.com>
2025-11-17 16:23:26 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyApdnTXHE7O6AoU9ldEV4yzsCVBGdsWuRg",
   authDomain: "web-appsmart-technology.firebaseapp.com",
   projectId: "web-appsmart-technology",
-  storageBucket: "web-appsmart-technology.firebasestorage.app",
   messagingSenderId: "595885713972",
   appId: "1:595885713972:web:9ab9dd4072db0101207861"
 };
@@ -14,4 +13,3 @@ firebase.initializeApp(firebaseConfig);
 // Servicios
 const auth = firebase.auth();
 const db = firebase.firestore();
-const storage = firebase.storage();
